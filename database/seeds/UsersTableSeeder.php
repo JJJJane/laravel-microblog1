0223331bb52b29e9901f647083fffd89f3177762
vagrant <vagrant@homestead>
2018-08-28 05:33:57 +0000

    Finish user edit,update,index, adn deestroy actions

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $users = factory(User::class)->times(50)->make();
+        User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
+
+        $user = User::find(1);
+        $user->name = 'Aufree';
+        $user->email = 'aufree@yousails.com';
+        $user->password = bcrypt('password');
+        $user->is_admin = true;
+        $user->save();
+    }
+}
